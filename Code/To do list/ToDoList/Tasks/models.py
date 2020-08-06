d2bdd5e1d1b367ac162ff49f85850de0a20f3f24
Nishu singh <imnishu23@gmail.com>
2020-08-06 20:22:08 +0530

    completed Project to-do list

--- Code/To do list/ToDoList/Tasks/models.py
+++ Code/To do list/ToDoList/Tasks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Tasks(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
--- Code/To do list/ToDoList/Tasks/views.py
+++ Code/To do list/ToDoList/Tasks/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 
 def index(request):
-    return render(request, 'Tasks/list.html')
+    Tasks = Task.objects.all()
 
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'Tasks': Tasks, 'form': form}
+
+    return render(request, 'Tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'Tasks/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'Tasks/delete.html',context)
